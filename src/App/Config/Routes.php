5ea764d4a591b1b2c1f466e5cc0fab257c0c5714
Reace Ian Roeloffze <reaceianroeloffze@gmail.com>
2026-02-13 21:07:46 +0200

    Create Routes class for registering view routes

---
+++ src/App/Config/Routes.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Config;
+
+use Framework\App;
+use App\Controllers\{HomeController, AboutController};
+
+/**
+ * Handle the registration of routes
+ * */
+class Routes
+{
+    /**
+     * Register all routes for the application
+     * */
+    public static function register(App $app): void
+    {
+        // Homepage
+        $app->getRoutePath(
+            '/',[HomeController::class, 'home']
+        );
+        // About page
+        $app->getRoutePath('/about', [AboutController::class, 'about']);
+    }
+}
