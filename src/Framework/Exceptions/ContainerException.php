edb3702fb042baf7eb72c6ddac060d0bb9210448
Reace Ian Roeloffze <reaceianroeloffze@gmail.com>
2026-02-14 13:25:14 +0200

    Add error handling for dependency injection

---
+++ src/Framework/Exceptions/ContainerException.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Framework\Exceptions;
+
+use Exception;
+
+/**
+ * Handle exceptions thrown by the container
+ * */
+class ContainerException extends Exception
+{
+}
